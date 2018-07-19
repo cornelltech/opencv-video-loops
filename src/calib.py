1bf22524f9b0abe898c4d562d03311cca3b2f880
D <doron@tracktunes.org>
2018-07-19 18:52:32 -0400

    minor

--- src/calib.py
+++ src/calib.py
@@ -3,11 +3,18 @@
 
 import sys
 import cv2
+import numpy as np
 from gray import Gray
 
 
 N_CORNERS = (7, 7)
 CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+objp = np.zeros((N_CORNERS[0] * N_CORNERS[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:N_CORNERS[0], 0:N_CORNERS[1]].T.reshape(-1, 2)
+
+obj_pts = []
+img_pts = []
 
 class Canny(Gray):
     """Calib"""
@@ -20,7 +27,6 @@ class Canny(Gray):
 
         # if found corners, overlay them on the image
         if ret == True:
-
             cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), CRITERIA)
 
             cv2.drawChessboardCorners(frame, N_CORNERS, corners, ret)
