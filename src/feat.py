0630ac5ad07f821d4c105f9a54b843ee6c744b7d
D <doron@tracktunes.org>
2018-07-19 10:54:22 -0400

    added

--- src/feat.py
+++ src/feat.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-"""knn.py"""
+"""feat.py - video streaming SIFT-like feature detection"""
 
 import sys
 import cv2
 from gray import Gray
 
 
-class KNN(Gray):
-    """KNN background subtraction"""
+class Feat(Gray):
+    """Feat: class for video streaming SIFT-like feature detection"""
 
     def __init__(self, stream, *args, **kwargs):
         super().__init__(stream, *args, **kwargs)
-        self.fgbg = cv2.createBackgroundSubtractorKNN(10, 500.0, False)
+        if 'method' in kwargs:
+            print('method: ', method)
 
     def process_frame(self, frame):
         """KNN background subtraction"""
-        gray = super().process_frame(frame)
-        return self.fgbg.apply(gray)
+        return frame
 
 
 if __name__ == '__main__':
-    KNN(cv2.VideoCapture(sys.argv[1])).start()
+    Feat(cv2.VideoCapture(sys.argv[1])).start()
