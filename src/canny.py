b298a232e4ff80352ebb23f595217eb73dab8114
D <doron@tracktunes.org>
2018-07-19 18:00:41 -0400

    minor

---
+++ src/canny.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""canny.py - video streaming SIFT-like cannyure detection"""
+
+import sys
+import cv2
+from gray import Gray
+
+
+class Canny(Gray):
+    """Canny: class for video streaming SIFT-like cannyure detection"""
+
+    def process_frame(self, frame):
+        """Cannyure detection per frame"""
+        return cv2.Canny(frame, 50, 150)
+
+
+if __name__ == '__main__':
+    Canny(sys.argv[1]).start()
