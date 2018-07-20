b8e3cc06cda5dcdb42bfbf95f629520e8fc76949
D <doron@tracktunes.org>
2018-07-20 16:11:22 -0400

    works

--- src/calib.py
+++ src/calib.py
@@ -9,18 +9,15 @@ from gray import Gray
 
 N_CORNERS = (7, 7)
 CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-OUTPUT_FILENAME = 'calib'
+CALIB_RESULTS_FILENAME = 'calib'
+OBJP = np.zeros((N_CORNERS[0] * N_CORNERS[1], 3), np.float32)
+OBJP[:, :2] = np.mgrid[0:N_CORNERS[0], 0:N_CORNERS[1]].T.reshape(-1, 2)
 
 class Canny(Gray):
     """Calib"""
 
     def __init__(self, stream, *args, **kwargs):
         super().__init__(stream, *args, **kwargs)
-
-        objp = np.zeros((N_CORNERS[0] * N_CORNERS[1], 3), np.float32)
-        objp[:, :2] = np.mgrid[0:N_CORNERS[0], 0:N_CORNERS[1]].T.reshape(-1, 2)
-        self.objp = objp
-
         self.accum_obj_pts = []
         self.accum_img_pts = []
         self.gray_shape = 0
@@ -36,9 +33,10 @@ class Canny(Gray):
 
         # if found corners, overlay them on the image
         if ret == True:
-            self.accum_obj_pts.append(self.objp)
-            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), CRITERIA)
-            self.accum_img_pts.append(corners)
+            self.accum_obj_pts.append(OBJP)
+            corners_subpix = cv2.cornerSubPix(
+                gray, corners, (11, 11), (-1, -1), CRITERIA)
+            self.accum_img_pts.append(corners_subpix)
             cv2.drawChessboardCorners(frame, N_CORNERS, corners, ret)
 
         return frame
@@ -49,8 +47,8 @@ class Canny(Gray):
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
             self.accum_obj_pts, self.accum_img_pts, self.gray_shape, None, None)
 
-        np.savez(OUTPUT_FILENAME, 
-                 mtx = mtx, dist = dist, rvecs = rvec, tvecs = tvecx)
+        np.savez(CALIB_RESULTS_FILENAME,
+                 mtx = mtx, dist = dist, rvecs = rvecs, tvecs = tvecs)
 
         super().stop()
 
